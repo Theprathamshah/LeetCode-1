5b570d228fd8d9a548b59617a2564fcbc2e35f44
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-12 14:31:18 -0800

    Time: 24 ms (5.82%), Space: 11.3 MB (27.32%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,37 +17,42 @@ public:
 class Solution {
     
 private:
-    unordered_map<Node*, Node*> _visited;
+    unordered_map<Node*, Node*> _visited; // Track oldNode -> newNode maping
+	
+	/*  -------------------------------------------------	
+	 *   Input: Pointer to a node in the original list        
+	 *   Output: Pointer to a node in the cloned list	  
+	 * -------------------------------------------------  */	 
     Node* getClonedNode(Node* oldNode) {
         if (oldNode == NULL) { return NULL; }
         
-        if (_visited.count(oldNode) == 0) {
+        if (_visited.count(oldNode) == 0) { 
+		    // If the node has not been visited, then create a node which will belong to the cloned list
             _visited[oldNode] = new Node(oldNode->val);            
-        }
-        
+        }        
         return _visited[oldNode];        
     }
 public:
     Node* copyRandomList(Node* head) {
         
         if (head == NULL) { return NULL; }
-        
-        Node* newHead = new Node(head->val);
-        
-        _visited[head] = newHead;
-        
+       
         Node* current = head;
         Node* newCurrent = NULL;
         
-        while(current != NULL) {
+        while(current != NULL) { // Iterative over the original list
             
-            newCurrent = getClonedNode(current);
+            // Get the cloned node beloning to the cloned list
+			newCurrent = getClonedNode(current);
+			
+			// Update the next and random pointers
             newCurrent->next = getClonedNode(current->next);
             newCurrent->random = getClonedNode(current->random);
             
-            current = current->next;            
+            // Go to the next node
+			current = current->next;            
         }
         
-        return newHead;        
+        return _visited[head];        
     }
 };

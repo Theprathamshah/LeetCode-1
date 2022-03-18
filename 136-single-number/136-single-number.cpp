2bdc334a6b7526127b952126e5b52d63e8c5bc7e
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-18 09:36:07 -0700

    Time: 23 ms (66.87%), Space: 16.9 MB (85.52%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int result = 0;
+        for (auto& num : nums) {            
+            result = result ^ num;            
+        }
+        
+        //  every element appears twice will be zeroed above.
+        // result will store just the number which doesnt appear tweice
+        
+        return result;
+        
+    }
+};
+
+/*
+
+XOR everything
+
+010
+010
+----
+
+
+000
+010
+---
+010
+010
+
+001
+
+
+
+
+
+
+
+
+
+
+*/

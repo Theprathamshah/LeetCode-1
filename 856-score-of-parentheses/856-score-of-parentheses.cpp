5a6a0a2c1776bae6d303fd13af9e3c0cd466947b
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-17 19:03:17 -0700

    Time: 0 ms (100.00%), Space: 6.1 MB (83.00%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,45 +1,38 @@
+
+/*
+
+DO AGAIN
+
+*/
+
+
+
 class Solution {
 public:
     int scoreOfParentheses(string s) {
         
         stack<int> sumAtVariousLevels;
-        
-        int sumAtLevel0 = 0; // this is the final answer
-        
-        int sumAtCurrentLevel = 0, sumAtPreviousLevel = 0;
-        int currentSum = 0;        
+        int sumAtLevelZero = 0;
+        sumAtVariousLevels.push(sumAtLevelZero); // this is the final sum
+               
+        int sumAtCurrentLevel = 0, sumAtPreviousLevel = 0;                
         for (auto& bracket : s) {            
-            if (bracket == '(') // start of a new level
-            {
-                currentSum = 0;
-                sumAtVariousLevels.push(currentSum);                
-            }
-            else
-            {                                        
-                sumAtCurrentLevel = sumAtVariousLevels.top(); sumAtVariousLevels.pop();  
-                if(sumAtCurrentLevel == 0)
-                {
-                    sumAtCurrentLevel += 1;                    
-                }
-                
-                
-                // However, the curent level is ending..
-                if(sumAtVariousLevels.empty())
-                {
-                    sumAtLevel0 += sumAtCurrentLevel; // just add.. no factor of 2
-                }
-                else
-                {
-                    sumAtPreviousLevel = sumAtVariousLevels.top(); sumAtVariousLevels.pop();  
-                    sumAtPreviousLevel += sumAtCurrentLevel * 2;                    
-                    
-                    sumAtVariousLevels.push(sumAtPreviousLevel);    
-                }
-                
-            }
-
             
-        }
-        return sumAtLevel0;
+            if (bracket == '(') // start of a new level
+            {                
+                sumAtVariousLevels.push(0);                
+            }
+            else // end of current level
+            {                                        
+                sumAtCurrentLevel = sumAtVariousLevels.top(); sumAtVariousLevels.pop();                  
+                sumAtPreviousLevel = sumAtVariousLevels.top(); sumAtVariousLevels.pop(); 
+                
+                sumAtPreviousLevel += max(2 * sumAtCurrentLevel, 1); // sumAtCurrentLevel == 0 means we had something like (). And in this case, we will add 1 to the previous level. On all other cases, it will be 2* sumAtCurrentLevel
+                    
+                sumAtVariousLevels.push(sumAtPreviousLevel);    
+                }                
+            }
+        sumAtLevelZero = sumAtVariousLevels.top(); sumAtVariousLevels.pop();  
+        return sumAtLevelZero;
     }
 };

02c3d5ba4b82c9347d0ba4211197865e897ed73b
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-20 12:04:06 -0700

    Time: 4 ms (34.96%), Space: 7.6 MB (76.16%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -7,17 +7,37 @@ public:
         
         if(n == 0) return 0;
         if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);;
         
-        vector<int> dp(n,0);
+//         vector<int> dp(n,0);
         
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+//         dp[0] = nums[0];
+//         dp[1] = max(nums[0], nums[1]);
+        
+//         for (int i = 2; i < n; i++) {
+//             dp[i] = max(               
+//                 dp[i-2] + nums[i], 
+//                 dp[i-1]);
+//         }
+//         return dp[n-1];
+        
+        int nMinus2 = nums[0];
+        int nMinus1= max(nums[0], nums[1]);
+        int nMinus0 = -1;
         
         for (int i = 2; i < n; i++) {
-            dp[i] = max(               
-                dp[i-2] + nums[i], 
-                dp[i-1]);
+            
+            nMinus0 = max(nMinus2 + nums[i], nMinus1);
+            
+            // Move by 1
+            nMinus2 = nMinus1;
+            nMinus1 = nMinus0;
         }
-        return dp[n-1];
+        
+        return nMinus0;
+            
+            
+        
+        
     }
 };

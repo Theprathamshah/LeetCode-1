67e043533533fed93927b8495c203f4a9f7ffb8e
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-20 20:43:33 -0700

    Time: 11 ms (18.02%), Space: 6.7 MB (64.08%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -2,52 +2,30 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         
-        unordered_map<char,int> freq;
+        vector<int> lastSeen(26,-1);
         
-        // populate freq.
-        for(auto& ch: s) {
-            freq[ch]++;
-        }
+        for(int i = 0; i < s.size(); i++) { lastSeen[s[i] - 'a'] = i; }
         
-        set<char> unique;
-        char ch;
+        vector<int> partitionLengths;
+        int curLen = 0;
         
-        int partitionLen = 0, countNonZeroInPartition = 0;
-        vector<int> lengthsOfPartitions;
-        
-        for(int i = 0; i < s.size(); i++) {
+        int curStartIdx = 0, curEndIdx = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            curEndIdx = max(curEndIdx, lastSeen[s[i] - 'a']);
             
-            ch = s[i];
-            
-            partitionLen++;
-            
-            countNonZeroInPartition = 0;
-            
-            // start of a new partition
-            unique.insert(ch);
-            
-            //Decrement from the freq map
-            freq[ch]--;
-            
-            int countInParitiion;
-            for(auto& c : unique) {
-                countNonZeroInPartition+= freq[c];
-                if (countNonZeroInPartition!= 0)
-                {
-                    break;
-                }                
-            }
-            
-            if(countNonZeroInPartition == 0) // all elements found are in this partition
-            {
-                unique.clear();
+            if(i == curEndIdx) { // we have reached the end of the partition
                 
-                lengthsOfPartitions.push_back(partitionLen);
-                partitionLen = 0;
+                curLen = curEndIdx - curStartIdx + 1;
+                partitionLengths.push_back(curLen);
+                
+                curStartIdx = i+1;
+                curEndIdx = i+1;
+                
             }
+                
+        }
+        return partitionLengths;
             
-        }        
-        
-        return lengthsOfPartitions;
     }
 };

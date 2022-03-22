8356695302d18f2617d35d2e2c7efcf224cd7828
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-21 17:14:45 -0700

    Time: 111 ms (33.79%), Space: 32.1 MB (78.21%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        
+        vector<int>& sortedArr = arr;
+        sort(sortedArr.begin(), sortedArr.end()); // O(NlogN)
+        
+        // find the minPositiveDelta
+        int minPositiveDelta = INT_MAX;
+        int tempDelta;
+        for (int i = 1; i < sortedArr.size(); i++) { // O(N)
+            tempDelta = sortedArr[i] - sortedArr[i-1];
+            minPositiveDelta = min(minPositiveDelta, tempDelta);
+        }
+        
+        vector<vector<int>> answer;
+        vector<int> element;
+        for (int i = 1; i < sortedArr.size(); i++) { // O(N)
+            tempDelta = sortedArr[i] - sortedArr[i-1];
+            if (tempDelta == minPositiveDelta) {
+                // Save element
+                element.emplace_back(sortedArr[i-1]); element.emplace_back(sortedArr[i]);
+                
+                // Update answer
+                answer.emplace_back(element);
+                
+                //Clear element
+                element.clear();                
+            }            
+        }
+        return answer;
+        
+        
+        
+    }
+};

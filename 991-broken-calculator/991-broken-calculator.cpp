7705738c4de9f369be352f21f4de8bdb60a3473b
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-22 22:34:46 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (30.21%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -3,12 +3,12 @@ public:
     int brokenCalc(int startValue, int target) {
         int steps = 0;
         while(target > startValue){
-            if(target % 2 == 0) {
-                target /= 2;                
+            if(target & 1) {
+                target++;                 
             }
             else
             {
-                target++;                
+                target /= 2;                               
             }
             steps++;
         }

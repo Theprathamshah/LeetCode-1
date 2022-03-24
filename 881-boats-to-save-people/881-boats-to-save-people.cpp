9aaa78f319a2d9bb2ca0e2736ab88d9191de07d7
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-23 19:54:25 -0700

    Time: 68 ms (97.18%), Space: 42 MB (10.43%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -3,19 +3,19 @@ public:
     int numRescueBoats(vector<int>& people, int limit) {
         
         // Sort in decending order
-        sort(people.begin(), people.end(), greater<int>()); // O(NlogN)
+        sort(people.begin(), people.end()); // O(NlogN)
         
         // Pick the largest and the 2nd largest..
-        int bigIdx = 0;
-        int smallIdx = people.size() - 1;
+        int smallIdx  = 0;
+        int bigIdx = people.size() - 1;
         
         int nBoats = 0;
-        while(bigIdx <= smallIdx) {            
-            if (people[bigIdx] + people[smallIdx] <= limit) {                
-                smallIdx--;
+        while(smallIdx <= bigIdx) {            
+            if (people[smallIdx] + people[bigIdx] <= limit) {      // basically if i can't pair up a fat guy with a really lean guy then the fat guy needs a boat of his own          
+                smallIdx++;
                 
             }
-            bigIdx++;                                  
+            bigIdx--;                                  
             nBoats++;
         }
         return nBoats;

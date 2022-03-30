12569999cd6263c2eed451bf4f36aceb3c7ccbf9
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-30 01:31:50 -0700

    Time: 12 ms (78.60%), Space: 7.7 MB (81.79%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> chars(128);
-
-        int left = 0;
-        int right = 0;
-
-        int res = 0;
-        while (right < s.length()) {
-            char r = s[right];
-            chars[r]++;
-
-            while (chars[r] > 1) {
-                char l = s[left];
-                chars[l]--;
-                left++;
+        
+        int startIdx = 0;
+        int endIdx = 0;
+        vector<int> map(128,-1); // <char, lastseenIdx>
+        char ch;
+        int maxSize = 0;
+        for (int endIdx = 0; endIdx < s.size(); endIdx++) {
+            ch = s[endIdx];
+            if(map[ch] >= 0) { //present
+                
+                if (startIdx <= map[ch]) { // means we need to update startIdx to move to the next idx
+                    startIdx = map[ch] + 1;
+                }
+                
             }
-
-            res = max(res, right - left + 1);
-
-            right++;
+            // also need to update the last seen idx
+            map[ch] = endIdx; 
+            
+            maxSize = max(maxSize, endIdx - startIdx + 1);
+            
         }
-
-        return res;
+        return maxSize;
     }
 };

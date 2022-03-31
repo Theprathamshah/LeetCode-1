aadf9d4ec42f22bed7406cecf4986c1d647500db
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-31 12:54:57 -0700

    Time: 3 ms (97.48%), Space: 6.7 MB (57.56%) - LeetHub

---
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
@@ -0,0 +1,65 @@
+class FizzBuzz {
+private:
+    int n;
+    int count;
+    mutex m;
+    condition_variable cv;
+
+public:
+    FizzBuzz(int n) {
+        this->n = n;
+        this->count = 1;
+    }
+
+    void fizz(function<void()> printFizz) {
+        while (count <= n) {
+            unique_lock<mutex> lock(m);
+            cv.wait(lock, [&](){ return count <= n and (count % 3 != 0 or count % 5 == 0) ? false : true;});     
+            if (count > n) break;
+            printFizz();
+            ++count;
+            lock.unlock();
+            cv.notify_all();
+        }
+        return;
+    }
+
+    void buzz(function<void()> printBuzz) {
+        while (count <= n) {
+            unique_lock<mutex> lock(m);
+            cv.wait(lock, [&](){ return count <= n and (count % 5 != 0 or count % 3 == 0) ? false : true;});            
+            if (count > n) break;
+            printBuzz();
+            ++count;
+            lock.unlock();
+            cv.notify_all();
+        }
+        return;
+    }
+
+	void fizzbuzz(function<void()> printFizzBuzz) {
+        while (count <= n) {
+            unique_lock<mutex> lock(m);
+            cv.wait(lock, [&](){ return count <= n and (count % 5 != 0 or count % 3 != 0) ? false : true;});        
+            if (count > n) break;
+            printFizzBuzz();
+            ++count;
+            lock.unlock();
+            cv.notify_all();
+        }
+        return;
+    }
+
+    void number(function<void(int)> printNumber) {
+        while (count <= n) {
+            unique_lock<mutex> lock(m);
+            cv.wait(lock, [&](){ return count <= n and (count % 5 == 0 or count % 3 == 0) ? false : true;});            
+            if (count > n) break;
+            printNumber(count);
+            ++count;
+            lock.unlock();
+            cv.notify_all();
+        }
+        return;
+    }
+};

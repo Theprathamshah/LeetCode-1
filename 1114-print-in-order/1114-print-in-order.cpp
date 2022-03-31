a415c8d586c214c06edb1aa558482b346201f62a
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-30 20:48:03 -0700

    Time: 164 ms (18.69%), Space: 7 MB (89.55%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,49 @@
+class Foo {
+public:
+    Foo() {
+        isfirstDone = false;
+        isSecondDone = false;        
+    }
+
+    void first(function<void()> printFirst) {
+        
+        auto gLock = unique_lock<mutex>(_m);
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        
+        isfirstDone = true;
+        gLock.unlock();
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        
+        auto gLock = unique_lock<mutex>(_m);
+        cv.wait(gLock, [&](){return isfirstDone;});
+        
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        
+        isSecondDone = true;
+        gLock.unlock();
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        
+        auto gLock = unique_lock<mutex>(_m);
+        cv.wait(gLock, [&](){return isSecondDone;});
+        
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+        
+        gLock.unlock();
+        cv.notify_all();
+    }
+private:
+    mutex _m;
+    condition_variable cv;    
+    bool isfirstDone;
+    bool isSecondDone;
+};

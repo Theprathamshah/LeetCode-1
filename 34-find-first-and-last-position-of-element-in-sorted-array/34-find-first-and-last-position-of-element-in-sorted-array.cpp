cf30a52aaedd6772d0ee4fa3fc8f432cdae4cb0f
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-03-31 22:47:02 -0700

    Time: 22 ms (5.39%), Space: 13.7 MB (65.34%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,35 +1,59 @@
 class Solution {
+
 private:
-    bool found(vector<int>& nums, int target) { // binary search O(n)
+    bool found(vector<int>& nums, int target) { // O(logN)        
+        int sIdx = 0;
+        int eIdx = nums.size() - 1;
+        int mIdx = -1;
+        bool isPresent = false;
         
-        int s = 0;
-        int e = nums.size()-1;
-        int m;
-        while(s <=e) {
-            m = s + (e-s) / 2;
-            if(nums[m] == target) { return true;}
-            else if (nums[m] > target) {
-                e = m - 1;
+        while(sIdx <= eIdx) {
+            mIdx = sIdx + (eIdx - sIdx) / 2;
+            
+            if (nums[mIdx] == target) {
+                isPresent = true;
+                break;
             }
-            else {
-                s = m + 1;
+            else if (nums[mIdx] < target) { 
+                sIdx = mIdx + 1;
             }
+            else { 
+                eIdx = mIdx - 1;            
         }
-        return false;
-        
     }
-    public:
+    return isPresent;
+    }
+    
+    int getLowerBoundIdx(vector<int>& nums, int target) { // O(logN)        
+        int sIdx = 0;
+        int eIdx = nums.size() - 1;
+        int mIdx = -1;
+        bool isPresent = false;
+        
+        while(sIdx <= eIdx) {
+            mIdx = sIdx + (eIdx - sIdx) / 2;
+                        
+            if (nums[mIdx] < target) { 
+                sIdx = mIdx + 1;
+            }
+            else { 
+                eIdx = mIdx - 1;            
+            }   
+        }
+        return sIdx;
+    }
+    
+public:
     vector<int> searchRange(vector<int>& nums, int target) {
         
-        auto ans = vector<int>{-1,-1};
-        if(found(nums,target)) {
-            auto first_it = lower_bound(nums.begin(), nums.end(), target);
-            auto last_it = upper_bound(nums.begin(), nums.end(), target);    
-            ans[0] = first_it - nums.begin();
-            ans[1] = --last_it  - nums.begin();
+        auto answer = vector<int>{-1,-1};
+        bool isPresent = found(nums,target);
+        if(!isPresent) {
+            return answer;
         }
-        return ans;
+        answer[0] = getLowerBoundIdx(nums,target);
+        answer[1] = getLowerBoundIdx(nums,target+1) - 1;
         
-        
+        return answer;
     }
 };

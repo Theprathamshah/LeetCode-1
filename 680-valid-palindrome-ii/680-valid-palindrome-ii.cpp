de39edf9a137a7765cb897105770552ab2f9536a
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-01 22:50:04 -0700

    Time: 64 ms (67.98%), Space: 21 MB (56.13%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    bool helper(string& s, int l, int h, bool chanceLeft = true) {
+        
+        if (l >=h) {
+            return true;
+        }
+        
+        bool answer = false;
+        if (s[l] == s[h]) {
+            answer |= helper(s,l+1,h-1,chanceLeft);
+        }
+        else if (!chanceLeft) {
+            return false;
+        }
+        else
+        {
+            chanceLeft = false;                
+            if (s[l+1] == s[h]) {
+                answer |= helper(s, l+1, h, chanceLeft);
+            }        
+            if (s[l] == s[h-1]) {
+                answer |= helper(s, l, h-1, chanceLeft);
+            }
+            
+        }
+        
+        
+        return answer;        
+    }
+public:
+    bool validPalindrome(string s) {
+        
+        int l = 0;
+        int h = s.size() - 1;
+        
+        return helper(s,l,h);
+     }
+};
+
+// "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"
+// "eeccccbebaeeabebccceea"

6e81f8884a5abaeb14e717e4339e567bff5026f2
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-01 23:24:49 -0700

    Time: 68 ms (54.06%), Space: 21.2 MB (56.11%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -2,27 +2,33 @@ class Solution {
 private:
     bool helper(string& s, int l, int h, bool chanceLeft = true) {
         
-        if (l >=h) {
+        // Bas condition
+        if (l >= h) {
             return true;
         }
         
         bool answer = false;
-        if (s[l] == s[h]) {
-            answer |= helper(s,l+1,h-1,chanceLeft);
+        if (s[l] == s[h]) 
+        { // If the first and last characters match, we rely on recursion to get us the answer to the smaller problem
+
+            answer |= helper(s,l+1,h-1,chanceLeft); 
         }
-        else if (!chanceLeft) {
+        else if (!chanceLeft) 
+        { // If the first and last characters DON'T match, and we have no chance left, then we don't need to go any further and we can directly return false. 
             return false;
         }
-        else
-        {
+        else 
+        {   // If the first and last characters DON'T match, but we have a chance left, then we will explore both cases. Either one returns true, we are good!
+            
             chanceLeft = false;                
-            if (s[l+1] == s[h]) {
+            if (s[l+1] == s[h]) 
+            { // CASE I - if moving first by 1 to the right and last match, then we will explore this branch
                 answer |= helper(s, l+1, h, chanceLeft);
             }        
-            if (s[l] == s[h-1]) {
+            if (s[l] == s[h-1]) 
+            {  // CASE II - if moving last by 1 to the left and first match, then we will "ALSO" explore this branch
                 answer |= helper(s, l, h-1, chanceLeft);
-            }
-            
+            }            
         }
         return answer;        
     }
@@ -36,7 +42,3 @@ public:
      }
 };
 
-// "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"
-// "eeccccbebaeeabebccceea"
-// "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"
-// "eeccccbebaeeabebccceea"

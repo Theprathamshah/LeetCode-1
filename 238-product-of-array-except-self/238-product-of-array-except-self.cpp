54aeb5c6c4f379601817c5386ac3b187b21f860b
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-06 15:48:43 -0700

    Time: 36 ms (43.88%), Space: 24.1 MB (56.28%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -4,21 +4,31 @@ public:
         
         int n = nums.size();
         
-        vector<int> leftProductExceptSelf(n, 1); // Space: O(N)        
+        vector<int> finalProductExceptSelf(n,1); // we'l; not count this in terms of space
         for(int i = 1; i < n; i++) { // Time: O(N)
-            leftProductExceptSelf[i] = leftProductExceptSelf[i-1] * nums[i-1];
+            finalProductExceptSelf[i] = finalProductExceptSelf[i-1] * nums[i-1];
         }
          
-        vector<int> rightProductExceptSelf(n, 1);
-        for(int i = n-2; i >= 0; i--) {
-            rightProductExceptSelf[i] = rightProductExceptSelf[i+1] * nums[i+1];
+        // vector<int> rightProductExceptSelf(n, 1);
+        // for(int i = n-2; i >= 0; i--) {
+        //     rightProductExceptSelf[i] = rightProductExceptSelf[i+1] * nums[i+1];
+        // }
+        
+        // In the space optimized approvh we will build R on the fly..
+        int rightProductExceptSelf = 1;
+        
+        // again here also, we will start from right side.. and keep updating rightProductExceptSelf as we go..
+        for(int i = n-1; i >= 0; i--) {
+            finalProductExceptSelf[i] = finalProductExceptSelf[i] * rightProductExceptSelf;
+            rightProductExceptSelf *= nums[i];
         }
         
-        vector<int> finalProductExceptSelf(n,1);
-        // for(int i = 0; i < n; i++) {
-        //     finalProductExceptSelf[i] = leftProductExceptSelf[i] * rightProductExceptSelf[i];
-        // }        
-        transform(leftProductExceptSelf.begin(), leftProductExceptSelf.end(), rightProductExceptSelf.begin(), finalProductExceptSelf.begin(), multiplies<int>());
+        
+        // vector<int> finalProductExceptSelf(n,1); // we'l; not count this in terms of space
+        // // for(int i = 0; i < n; i++) {
+        // //     finalProductExceptSelf[i] = leftProductExceptSelf[i] * rightProductExceptSelf[i];
+        // // }        
+        // transform(leftProductExceptSelf.begin(), leftProductExceptSelf.end(), rightProductExceptSelf.begin(), finalProductExceptSelf.begin(), multiplies<int>());
         
         return finalProductExceptSelf;
     }

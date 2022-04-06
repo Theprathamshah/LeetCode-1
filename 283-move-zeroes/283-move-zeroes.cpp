4ad3fbab037a20929f823edfcd33518e2ee41ac6
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-06 14:17:34 -0700

    Time: 26 ms (73.14%), Space: 19.5 MB (8.20%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,32 +2,6 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
-        int nextNonzeroIdx = 0;
-        int currIdx = 0;
-        
-        for(int currIdx = 0; currIdx < nums.size(); currIdx++) {  // O(n) time O(1) space
-            
-            if(nums[currIdx]) { // non zero value found
-                
-                // swap with nextNonzeroIdx                
-                // swap(nums[nextNonzeroIdx], nums[currIdx]);
-                
-                // Instead of swapping (which is expensive), lets just copy it over..
-                //nums[nextNonzeroIdx] = nums[currIdx];                
-                // incrememnt nextNonzeroIdx so now nextNonzeroIdx points to the next locaiton where the val should go
-                //nextNonzeroIdx++;
-                
-                // Copying and increment can be combined..
-                nums[nextNonzeroIdx++] = nums[currIdx];   
-            }        
-        }
-        
-        
-        // now everything from nextNonzeroIdx till the end can be a zero, right?
-        fill(nums.begin() + nextNonzeroIdx, nums.end(), 0);
-        
-        
-        
-        return;
+       stable_partition(nums.begin(), nums.end(), [](int item){return item!=0; });
     }
 };

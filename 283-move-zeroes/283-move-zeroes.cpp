e39d0bfd25e42cf73375fb678958d80770f6a156
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-06 10:09:59 -0700

    Time: 34 ms (47.77%), Space: 19.3 MB (57.79%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -9,13 +9,22 @@ public:
             
             if(nums[currIdx]) { // non zero value found
                 
-                // swap with nextNonzeroIdx
+                // swap with nextNonzeroIdx                
+                // swap(nums[nextNonzeroIdx], nums[currIdx]);
+                
+                // Instead of swapping (which is expensive), lets just copy it over..
+                nums[nextNonzeroIdx] = nums[currIdx];
+                
                 // incrememnt nextNonzeroIdx so now nextNonzeroIdx points to the next locaiton where the val should go
-                swap(nums[nextNonzeroIdx], nums[currIdx]);
                 nextNonzeroIdx++;
             }
             currIdx++;            
         }
+        
+        // now everything from nextNonzeroIdx till the end can be a zero, right?
+        fill(nums.begin() + nextNonzeroIdx, nums.end(), 0);
+        
+        
         return;
     }
 };

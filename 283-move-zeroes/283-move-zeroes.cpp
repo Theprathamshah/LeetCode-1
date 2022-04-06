4ceb67770555b93abb2858b1c953b2b51c908e85
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-06 14:22:00 -0700

    Time: 32 ms (53.31%), Space: 19.7 MB (7.03%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
-       stable_partition(nums.begin(), nums.end(), [](int item){return item!=0; });
+        // 0 -> logical_not -> true.. will move to the begining..
+        // if we traverse the list from beginnning.. all zeros will move to the front..
+        // therfore, we should traverse the list from the back :)
+       stable_partition(nums.rbegin(), nums.rend(), logical_not<int>());
     }
 };

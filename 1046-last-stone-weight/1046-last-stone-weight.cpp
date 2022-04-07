199da3625831f7d280f5b8f4feee269d00239dea
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-06 18:08:08 -0700

    Time: 6 ms (15.97%), Space: 7.6 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -3,23 +3,17 @@ public:
     int lastStoneWeight(vector<int>& stones) {
         
        // Max HEAP
-        priority_queue<int> pq;
-        for(auto& stone : stones) {
-            pq.push(stone);
-        }
+        priority_queue<int> pq (stones.begin(), stones.end());
         
         // 1. pop top two elements
         // 2. Substract
         // 3. inssert in the pq
-        // 4. Repeat until the size is 1
-        
+        // 4. Repeat until the size is greater than 1        
         while(pq.size() > 1) {
             int h1 = pq.top(); pq.pop();
             int h2 = pq.top(); pq.pop();
             pq.push(h1-h2);
         }
-        int answer = pq.top(); pq.pop();
-        return answer;
-        
+        return pq.empty() ? 0 : pq.top();
     }
 };

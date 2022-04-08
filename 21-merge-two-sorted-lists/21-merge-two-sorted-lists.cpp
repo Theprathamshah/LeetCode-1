858202b60ef21261eac31669c57f9b56f317f32a
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-07 17:39:19 -0700

    Time: 12 ms (46.41%), Space: 14.8 MB (43.84%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -8,21 +8,62 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+//         if(!list1 and !list2) {return nullptr; }
+//         if(!list1) { return list2; }
+//         if(!list2) { return list1; }
+        
+//         if(list1->val < list2->val) {
+//             list1->next = mergeTwoLists(list1->next, list2);
+//             return list1;
+//         }
+//         else {
+//             list2->next = mergeTwoLists(list1, list2->next);
+//             return list2;
+//         }
+//     }
+// };
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
-        if(!list1 and !list2) {return nullptr; }
-        if(!list1) { return list2; }
-        if(!list2) { return list1; }
+        ListNode* sentinal = new ListNode(-101);
+        sentinal->next = nullptr;
         
-        if(list1->val < list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
+        ListNode* prev = sentinal;
+        
+        while(list1 and list2) { // we do all this compare business until both are present.. after that we will just connect to which ever is alive..
+          
+            if(list1 -> val < list2->val) {
+                prev->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                prev->next = list2;
+                list2 = list2->next;
+            }
+            prev = prev -> next;            
+        }
+        
+        if(list1) {
+            prev->next = list1;
+        }
+        else if(list2) {
+            prev->next = list2;
         }
         else {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
+            prev->next = nullptr;
         }
+        
+        ListNode* answer = sentinal->next;
+        delete sentinal;
+        
+        return answer;
+        
+  
     }
 };

f46b39a6dfa92260e702b9385013a18b092c51eb
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-08 19:00:18 -0700

    Time: 24 ms (42.68%), Space: 13.8 MB (22.19%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -3,33 +3,30 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {     
         
         // Create a "freq" map
+        // O(1) time
+        // O(n) space i think (?)
         unordered_map<int, int> freq; 
         for (auto& n : nums) {
             freq[n]++;
         }
-        
-        /*
-        1 -> 3
-        2 -> 3
-        3 -> 1
-        
-        */
-        
+
+        // Insert pair of <count, val> into the set which will be arranged in ascending order
+        // we will keep this set of size "k"
         set<pair<int,int>> s;
         for (auto& [val, count] : freq) {            
             s.insert(make_pair(count, val));
-            if (s.size() > k) {
+            if (s.size() > k) { // throw out the 1st element as it might have the least count
                 s.erase(s.begin());
             }            
         }
         
+        // once we have iterated over the freq map, then we need to iterate over the set from the back (largest will be at the back) and add to the answer.
+        // then we can remove that last elment and keep repeating this process until the set is empty
         vector<int> ans;
         while(!s.empty()) {
             ans.push_back(s.rbegin()->second);
             s.erase(*s.rbegin());
-        }
-        
-        
+        }        
         return ans;
         
         

58f974e0cce2f125b3e1c2cbae4aeac668d801a6
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-11 12:37:46 -0700

    Time: 8 ms (97.56%), Space: 7.7 MB (39.82%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -12,22 +12,20 @@ public:
         
         ListNode* curr = node; 
         ListNode* fwd = node; // It is given that the node can't be the last, so we can safely assume node->next is not NULL
+        ListNode* prev = nullptr; 
         
-        while(fwd->next) { // Walk till fwd is the last node
+        while(fwd) { 
             
             // Copy the value
             curr->val = fwd->val;
             
             // Move by 1 to the right
+            prev = curr;
             curr = fwd;
-            fwd = fwd->next;
+            fwd = fwd->next;            
         }
         
-        //copy one last time
-        curr->val = fwd->val;
-        
-        // fwd is the tail and curr points to fwd
-        curr->next = nullptr;
-        delete fwd;
+        prev->next = nullptr;
+        delete curr;
     }
 };

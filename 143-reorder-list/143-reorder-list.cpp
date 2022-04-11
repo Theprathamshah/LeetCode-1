d87aeef1e0bdeb8ee428f80b55bbf75055a672f4
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-10 21:25:45 -0700

    Time: 32 ms (99.15%), Space: 17.6 MB (95.87%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* getMid(ListNode* head) { // for a linked list of even size, return the 1st mid element        
+        if(!head or !head->next) {return head; }        
+        ListNode* fast = head;
+        ListNode* slow = head;        
+        while(fast->next and fast->next->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+
+    ListNode* reverse(ListNode* head) {
+        if(!head or !head->next) {return head; }        
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        ListNode* fwd = nullptr;
+
+        while(curr) {
+            fwd = curr->next;            
+            curr->next = prev;            
+            prev = curr;
+            curr = fwd;            
+        }
+        return prev;
+    }
+    
+public:
+    void reorderList(ListNode* head) {
+        
+        if(!head or !head->next) {return; }        
+        
+        ListNode* mid = getMid(head);
+        
+        ListNode* mid_next = mid->next;
+        mid->next = nullptr;
+        
+        ListNode* tail = reverse(mid_next);
+        
+        ListNode* head_temp = head;
+        ListNode* fwd_h, *fwd_t;
+        while(head_temp and tail) {
+            
+            // Save fwd
+            fwd_h = head_temp->next;
+            fwd_t = tail->next;
+            
+            // Update links
+            head_temp->next = tail;
+            tail->next = fwd_h;
+            
+            // Move
+            head_temp = fwd_h;
+            tail = fwd_t;            
+        }
+        return;
+        
+        
+    }
+};

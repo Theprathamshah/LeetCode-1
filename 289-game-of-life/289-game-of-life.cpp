f2a5de8513c04f548b3447e74c69827bbf500a11
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-11 18:06:46 -0700

    Time: 0 ms (100.00%), Space: 6.9 MB (84.44%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        
+        int m = board.size();
+        int n = board[0].size();
+        
+        vector<vector<int>> temp(m, vector<int>(n));
+        
+        vector<int> x_dir = {-1,-1, 0, 1 ,1,  1,  0, -1};
+        vector<int> y_dir = { 0, 1, 1, 1 ,0, -1, -1, -1};
+        
+        int x_temp;
+        int y_temp;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                
+                int count_0 = 0;
+                int count_1 = 0;                
+                
+                // Count live and dead neighbours
+                for(int z = 0; z < x_dir.size(); z++) {
+                    x_temp = i + x_dir[z]; 
+                    y_temp = j + y_dir[z]; 
+                    
+                    if( x_temp >= 0 and x_temp < m and y_temp >= 0 and y_temp < n) {
+                        board[x_temp][y_temp] == 0 ? count_0++ : count_1++;                    
+                    }                    
+                }
+                
+                // Take a snapshot
+                temp[i][j] = board[i][j];
+                
+                
+                if(board[i][j] == 1) { // Any live cell with - 
+                    //1. fewer than two live neighbors dies as if caused by under-population.
+                    //3. more than three live neighbors dies, as if by over-population.
+                    if(count_1 < 2 or count_1 > 3) {
+                        temp[i][j] = 0;
+                    }                    
+                } 
+                else { // 4. Any dead cell with -
+                    if(count_1 == 3) {
+                        temp[i][j] = 1;
+                    }
+                }
+            }
+        }
+        
+        // temp has the snapshop. copy it back to board
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                board[i][j] = temp[i][j];
+            }
+        }
+    }
+};

1d515d38eca94d51d6acc229b7f412734bd27b60
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-11 18:33:53 -0700

    Time: 4 ms (26.80%), Space: 7.1 MB (9.40%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -6,43 +6,48 @@ public:
         int n = board[0].size();
         
         vector<vector<int>> temp(m, vector<int>(n));
-        
-        vector<int> x_dir = {-1,-1, 0, 1 ,1,  1,  0, -1};
-        vector<int> y_dir = { 0, 1, 1, 1 ,0, -1, -1, -1};
-        
+                        
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                
-                int count_0 = 0;
+                                
                 int count_1 = 0;                
+                
+                //delta to go to neighbor
+                vector<int> x_dir = {-1,-1, 0, 1, 1, 1, 0,-1};
+                vector<int> y_dir = { 0, 1, 1, 1, 0,-1,-1,-1};
+                
                 
                 // Count live and dead neighbours
                 for(int z = 0; z < x_dir.size(); z++) {
         
+                    // Neighbor coordinate
                     int x_temp = i + x_dir[z]; 
                     int y_temp = j + y_dir[z]; 
                     
                     if( x_temp >= 0 and x_temp < m and y_temp >= 0 and y_temp < n) {
-                        board[x_temp][y_temp] == 0 ? count_0++ : count_1++;                    
+                        
+                        if(board[x_temp][y_temp] > 0){
+                            count_1++;
+                        }   
                     }                    
                 }
                 
-                // Take a snapshot
-                temp[i][j] = board[i][j];                
+                // Take a snapshot of the cell's value
+                // temp[i][j] = board[i][j];                
                 
-                // Update the result for the following
-                if(board[i][j] == 1) { 
+                // Update the snapshot for the following cases
+                if(board[i][j] > 0) { 
                     
                     // #1 and #3
                     if(count_1 < 2 or count_1 > 3) {
-                        temp[i][j] = 0;
+                        board[i][j] = 2;
                     }    
                     // #2 doesn't need to be handled seperately
                 } 
                 else { 
                     // #4
                     if(count_1 == 3) {
-                        temp[i][j] = 1;
+                        board[i][j] = -1;
                     }
                 }
             }
@@ -51,7 +56,12 @@ public:
         // temp has the snapshop. copy it back to board
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                board[i][j] = temp[i][j];
+                if(board[i][j] == 2){
+                    board[i][j] = 0;
+                }
+                else if(board[i][j] == -1) {
+                    board[i][j] = 1;
+                }
             }
         }
     }

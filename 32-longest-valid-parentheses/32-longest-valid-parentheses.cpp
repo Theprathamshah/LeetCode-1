e5ed899b7f18c13f17ce1cfb3ae9ac6b65ec688b
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-13 10:14:56 -0700

    Time: 4 ms (74.60%), Space: 6.8 MB (95.67%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -2,29 +2,39 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
                 
-        stack<int> openBracketIndexHolder;
+        int openBracketCounter = 0;
         
-        // Iterate once to find the index of brackets with no matching and mark them as 'x'
-        for (int idx = 0; idx < s.size(); idx++) { // O(n)
-            
+        // Iterate from front and mark ')' with no matching open brackets
+        for (int idx = 0; idx < s.size(); idx++) { // O(n)            
             if(s[idx] == ')') {
-                if (openBracketIndexHolder.empty()) {
-                    s[idx] = 'x'; // Mark ')' with no matching open brackets
+                if (openBracketCounter == 0) {
+                    s[idx] = 'x'; 
                 }
                 else {
-                    openBracketIndexHolder.pop();
+                    openBracketCounter--;
                 }
             }
             else {
-                openBracketIndexHolder.push(idx); // save idx of open brackets
-            }
+                openBracketCounter++ ;
+            }        
         }
         
-        while(!openBracketIndexHolder.empty()) {
-            int idx = openBracketIndexHolder.top(); openBracketIndexHolder.pop();
-            s[idx] = 'x';    // Mark '(' with no matching closing
-
+        // Iterate from back and mark '(' with no matching open brackets
+        openBracketCounter = 0;
+        for (int idx = s.size() - 1; idx >= 0; idx--) { // O(n)            
+            if(s[idx] == '(') {
+                if (openBracketCounter == 0) {
+                    s[idx] = 'x'; 
+                }
+                else {
+                    openBracketCounter--;
+                }
+            }
+            else {
+                openBracketCounter++;
+            }        
         }
+       
         
         // Now iterate another time and count pairs
         int maxCount = 0, runningCount = 0;
@@ -40,8 +50,6 @@ public:
                 runningCount += 2;
                 maxCount = max(maxCount, runningCount);
             }
-            
-            
         }
         return maxCount;
         

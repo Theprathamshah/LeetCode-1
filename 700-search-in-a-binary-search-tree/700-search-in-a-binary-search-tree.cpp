41d2502d249db3a71a9c9cf77d0063f0802238c3
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-13 19:41:49 -0700

    Time: 35 ms (95.74%), Space: 34.8 MB (66.88%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -14,6 +14,18 @@ public:
     TreeNode* searchBST(TreeNode* root, int val) {
         
         if(!root or root->val == val) { return root;}
-        return root->val > val ? searchBST(root->left, val) : searchBST(root->right, val);        
+        
+        stack<TreeNode*> mystack;
+        mystack.push(root);
+        while(!mystack.empty()) {
+            root = mystack.top(); mystack.pop();            
+            if(root->val == val) {return root;}
+            else if(val < root->val and root->left) { mystack.push(root->left); }
+            else if(val > root->val and root->right) { mystack.push(root->right); }
+            else { return nullptr; }            
+        }
+        return nullptr;
+        
+        
     }
 };

e5c15abd1046a015e1e0761a6f2ea9a0ecde0a69
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-13 19:10:03 -0700

    Time: 3 ms (56.61%), Space: 8.5 MB (44.95%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -10,30 +10,26 @@
  * };
  */
 class Solution {
+private:
+    vector<int> _answer;
+    
+    void helper(TreeNode* root) {
+        
+        if(!root) { return;}
+        
+        // Insert root first
+        _answer.push_back(root->val);
+        // Insert left child after root
+        helper(root->left);
+        // Insert right child after left
+        helper(root->right);
+    }
+    
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         
-        if(!root) {
-            vector<int> result = {};
-            return result;
-        }
-        
-        int val = root->val;
-        vector<int> left = preorderTraversal(root->left);
-        vector<int> right = preorderTraversal(root->right);
-        
-        // Re-allocate space big enough for the children and yourself
-        left.reserve(left.size() + right.size() + 1);
-        
-        // Insert val before left
-        left.insert(left.begin() ,val);
-        // Insert right after left
-        left.insert(left.end(), right.begin(), right.end());
-        
-        // root -> left -> right
-        
-        return left;
-        
+        helper(root);        
+        return _answer;
     }
 };
 

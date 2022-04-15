0e0af9ac50fad8198cf53841181cd378e8974bcb
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-14 20:43:31 -0700

    Time: 30 ms (36.82%), Space: 19.5 MB (97.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        unordered_map<int, int> freq;
+        unordered_map<int,int> freq;
+        int maxElementCount = 0, maxElement;
+        for_each(nums.begin(), nums.end(), [&](int n) { 
+            freq[n]++; 
+            if(maxElementCount < freq[n]) {
+                maxElementCount = freq[n];
+                maxElement = n;   
+            }
         
-        int N = nums.size();
+        });
         
-        int threshold = N/2;
-        
-        for (auto& num : nums) {
-            
-            
-            if(++freq[num] > threshold) { return num;}
-            
-        }
-        return -1;
-        
+        return maxElement;
     }
 };

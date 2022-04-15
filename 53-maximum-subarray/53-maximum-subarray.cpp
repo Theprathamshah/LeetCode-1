ec3813c48a07f58d101adef2eaa65c7c075d5d87
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-14 19:53:43 -0700

    Time: 118 ms (79.45%), Space: 67.7 MB (54.72%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int maxSum = INT_MIN;
+        int currentSum = INT_MIN;
+        
+        for(auto& n : nums) {
+            
+            if (n > currentSum and currentSum < 0) { // means we should start fresh
+                currentSum = n;                
+            }
+            else { // in all other cases we should update currentSum, even though "n" might be negative. that's coz the next number might be bigger than the current negative number eventually bringing up the current sum..
+                currentSum += n;
+                
+            }
+            maxSum = max(maxSum, currentSum);
+            
+        }
+        return maxSum;
+        
+    }
+};

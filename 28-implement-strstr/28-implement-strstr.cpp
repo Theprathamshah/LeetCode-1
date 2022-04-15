3c6a3a5c8b4e76eaedccbcd0f19122d2a9a8efa2
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-15 15:42:53 -0700

    Time: 4 ms (80.00%), Space: 6.2 MB (91.46%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -11,22 +11,26 @@ public:
         int idx = -1;
         while(start <= n-m) {
             idx = 0;
-            if(haystack[start] == needle[idx]) {
+            if(haystack[start] == needle[idx]) { // 1st character matched
                 idx++;
                 end = start+1;
-                while(end < n and idx < m) {
+                while(end < n and idx < m) { // ensure we check for 2nd and beyond only when it is within range
                     
-                    if(haystack[end] != needle[idx]) {                                                
+                    if(haystack[end] != needle[idx]) {               // if something doesn't match, then we need to start seearching again from start..                                 
                         break;
                     }
+                    
+                    // If things are matching, just keep incrementing the end in the haystack and idx in needle
                     end++;
                     idx++;
                 }
-                if(idx == m) {
+                
+                if(idx == m) { // this means we must have found the needle so we can happily return 
                     return start;        
                 }
                 
             } 
+            // incrment the start
             start++;
         }
         return -1;        

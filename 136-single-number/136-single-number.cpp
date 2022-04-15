2580c984a30eb34fba1c7289cb54831ea633a4ed
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-14 20:19:34 -0700

    Time: 20 ms (79.30%), Space: 17 MB (49.33%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        int answer = 0; //xor everything. Everything will cancel out with each other.. only the non-dulicate will remain
+        // int answer = 0; //xor everything. Everything will cancel out with each other.. only the non-dulicate will remain        
+        // for (auto&n: nums) {
+        //     answer ^= n;
+        // }
+        // return answer;
         
-        for (auto&n: nums) {
-            answer ^= n;
-        }
-        
-        return answer;
-        
-        
+        return accumulate(nums.begin(), nums.end(), 0, [](int x, int y){ return x^y; });
     }
 };

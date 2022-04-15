3039e9e54bb22e886b2134e6f272c6ec3aad4e37
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-15 10:47:53 -0700

    Time: 212 ms (73.73%), Space: 106.4 MB (12.09%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.cpp
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.cpp
@@ -5,6 +5,8 @@ public:
         // Definations
         int sumOfProducts = 0;
         
+        vector<vector<int>> map(100, vector<int>(10^5));
+        
         
         
         sort(nums1.begin(), nums1.end());
@@ -20,16 +22,18 @@ public:
 
 /*
 to minimize, we need to pair the largest in num1 with the smallest in num2..
-now the question is what is the best way to accomplish this task...? 
 
 
+.......... now the question is what is the best way to accomplish this task...? ...............
+
+1. we can "create" heap and all but that's insanly slow operation.. i mean its similar to sorting both of them right.. NlogN time
+
+2. I tried sorting them inplace.. and thats so much faster in terms of time and space..  however, the question says we can touch nums2..
+
+now clearly.. i need to do something inplace in nums1......... i think i might swapping the values so i dont loose it..
 
 
-we can create heap and all but that's insanly slow operation.. i mean its similar to sorting both of them right.. NlogN time
-
-
-
-
+[4] <-- let's say if i have the freq map.. i can 
 
 
 */

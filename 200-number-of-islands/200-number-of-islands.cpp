fe57b1e4bd7259bd6216175311a8aa0a3cf70948
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-16 13:33:59 -0700

    Time: 36 ms (84.30%), Space: 12.4 MB (45.89%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,28 +1,26 @@
 class Solution {
 private:
     int ROW_MIN, COL_MIN, ROW_MAX, COL_MAX;
-    bool isValidCell(vector<vector<char>>& grid, int i, int j) {        
+    bool isValidLand(vector<vector<char>>& grid, int i, int j) {        
         return (i >= ROW_MIN and i <= ROW_MAX and j >= COL_MIN and j <= COL_MAX and grid[i][j] == '1') ? true : false;        
     }
     void dfs(vector<vector<char>>& grid, int i, int j) {
         
-        if(!isValidCell(grid, i,j)) {return;}
+        // if(!isValidLand(grid, i,j)) {return;}
+            
+        // If we get here, means we are at a valid land. now we need to explore all the neighboring cells to see how big is our island.
         
-        // If this cell is water, we can simply return without doing anything
-        // if(grid[i][j] == '0') { return; }
-        
-        // If we get here, means we are at land. now we need to explore all the neighboring cells so se how big is our island.
-        
-        // first thing we will do is to mark ourselves as visiited
+        // first thing we will do is to mark ourselves as visited
         grid[i][j] = '0';
         
-        // Now lets explore our neighbors
+        // Now lets explore our neighbors. If we find a land, we will keep marking it as 0 so we don't keep going back and forth!
         array<int,4> di = {-1, 0, 1, 0};
-        array<int,4> dj = {0, 1, 0, -1};
-        
+        array<int,4> dj = {0, 1, 0, -1};        
         for(int k = 0; k < di.size(); k++) {
-            dfs(grid, i + di[k], j + dj[k]);
-        }
+            if(isValidLand(grid,i + di[k], j + dj[k])) {
+                dfs(grid, i + di[k], j + dj[k]);    
+            }            
+        }        
     }
     
     

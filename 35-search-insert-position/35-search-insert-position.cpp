e40d2d1dd18f011d1709340e6c703cd75e23be50
Astha Parikh <101363924+asthapk99@users.noreply.github.com>
2022-04-15 22:22:15 -0700

    Time: 6 ms (54.65%), Space: 9.7 MB (23.87%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -2,27 +2,21 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         
-        int lo = 0;
-        int hi = nums.size() - 1;
+        int lo = 0, hi = nums.size() - 1, mid;
         
-        int mid = -1;
-        
-        while (lo <= hi) {
+        while(lo <= hi) {
+            mid = lo + (hi-lo)/2;
             
-            mid = lo + (hi - lo) / 2;
-            
-            if (nums[mid] == target) {
+            if(nums[mid] == target) {
                 return mid;
-            }
-            else if (nums[mid] > target) // favorable region
-            {
-                hi = mid - 1;
+            }            
+            else if (nums[mid] < target) {
+                lo = mid + 1; 
             }
             else {
-                lo = mid + 1;
-            }
+                hi = mid - 1;
+            }                        
         }
-        return lo;
-        
+        return lo;        
     }
 };
